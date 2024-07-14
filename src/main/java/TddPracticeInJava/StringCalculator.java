4b8cc063be6d9a20b364045b1c7f52aa62cca05c
narendran <42309607+Narendrannr@users.noreply.github.com>
2024-07-15 00:12:47 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -3,13 +3,18 @@ package TddPracticeInJava;
 public class StringCalculator
 {
   public static int add(String inputStr) {
+    int sumOfTwo = -1;
+     if (checkInputEmpty(inputStr) || inputStr.equals("0")) {
+        return 0;
+    }
+    return sumOfTwo;
+  }
+
+  private static boolean checkInputEmpty(String inputStr){
     if (inputStr == null || inputStr == "") {
-        return 0;
+        return true;
     }
-     if (inputStr.equals("0")) {
-        return 0;
-    }
-    return -1;
+    return false;
   }
 }
 
